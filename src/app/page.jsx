859c098304c133d670d0517c85e340ffa3b9b363
Amrit Thapa <amritthapa966@gmail.com>
2024-01-04 18:53:23 +0530

    adding basic setup template (#3)

---
+++ src/app/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+const page = () => {
+  return (
+    <>
+      <div className="intro" >
+        introduction
+      </div>
+      <div className="scroll-animate exp" >
+        work experience.
+      </div>
+      <div className="scroll-animate edu" >
+        education
+      </div>
+      <div className="scroll-animate skill" >
+        skills
+      </div>
+      <div className="scroll-animate contact" >
+        contact section
+      </div>
+    </>
+  )
+}
+
+export default page;
