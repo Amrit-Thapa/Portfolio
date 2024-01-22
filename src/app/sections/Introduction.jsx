73e52c0c9841cfdda70adb0e1801ec92e6ddd087
Amrit Thapa <amritthapa966@gmail.com>
2024-01-22 22:58:12 +0530

    adding skill section (#7)

--- src/app/sections/Introduction.jsx
+++ src/app/sections/Introduction.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const Introduction = () => {
   return (
-    <div>
+    <div className="w-full">
       <div
         className={classNames(
-          "h-[600px] bg-black flex",
+          "h-[600px] bg-black flex w-screen",
           "items-center justify-center text-white flex-col",
         )}
       >
         <div className="p-2">
-          <div className="text-3xl lg:text-6xl text-center">Amrit Thapa</div>
+          <div className="text-3xl text-center lg:text-6xl">Amrit Thapa</div>
           <div className="text-xs md:text-lg">
             Software Engineer / Front-end Engineer
           </div>
