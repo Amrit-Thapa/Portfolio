2763684c8853485787d08b6885f707bb2916f6c8
Amrit Thapa <amritthapa966@gmail.com>
2024-01-22 22:57:33 +0530

    adding skill section

--- src/app/components/SectionBody.jsx
+++ src/app/components/SectionBody.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 export const SubHeading = ({children}) => {
-  return <div className="text-3xl pb-5">{children}</div>;
+  return <div className="pb-5 text-3xl">{children}</div>;
 };
 
 export const Paragraph = ({children}) => {
-  return <div className="leading-7 pb-3">{children}</div>;
+  return <div className="pb-3 leading-7">{children}</div>;
 };
 
 const SectionBody = ({children}) => {
   return (
     <div>
-      <div className="flex gap-14 max-w-[1140px] p-3 flex-wrap md:flex-nowrap m-auto">
+      <div className="flex gap-14 max-w-[1140px] w-full p-3 flex-wrap md:flex-nowrap m-auto">
         {children}
       </div>
     </div>
--- src/app/sections/Skills.jsx
+++ src/app/sections/Skills.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import SectionContainer from "../components/SectionContainer";
+import SectionHead from "../components/SectionHead";
+import SectionBody from "../components/SectionBody";
+import reactLogo from "../../../public/images/react.png";
+import jsLogo from "../../../public/images/javascript.png";
+import reduxLogo from "../../../public/images/redux.png";
+import typescriptLogo from "../../../public/images/typescript.png";
+import tailwindLogo from "../../../public/images/tailwindcss.png";
+import htmlLogo from "../../../public/images/html5.png";
+import css3Logo from "../../../public/images/css.png";
+import nextjsLogo from "../../../public/images/nextjs.png";
+import nodejsLogo from "../../../public/images/nodejs.png";
+import sassLogo from "../../../public/images/sass.png";
+import gitLogo from "../../../public/images/github.png";
+
+const SkillIconContainer = ({children}) => {
+  return (
+    <div className="p-2 rounded-lg cursor-pointer hover:shadow">{children}</div>
+  );
+};
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <SectionContainer>
+      <SectionHead>Skills</SectionHead>
+      <SectionBody>
+        <div className="flex flex-wrap justify-center w-full text-center md:gap-20">
+          <SkillIconContainer>
+            <Image src={reactLogo} alt="react" />
+          </SkillIconContainer>
+          <SkillIconContainer>
+            <Image src={jsLogo} alt="js" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={reduxLogo} alt="redux" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={typescriptLogo} alt="typescript" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={tailwindLogo} alt="tailwindLogo" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={htmlLogo} alt="htmlLogo" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={css3Logo} alt="css3Logo" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={nextjsLogo} alt="tailwindLogo" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={nodejsLogo} alt="tailwindLogo" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={sassLogo} alt="tailwindLogo" />
+          </SkillIconContainer>
+          <SkillIconContainer className="p-2 cursor-pointer hover:shadow">
+            <Image src={gitLogo} alt="tailwindLogo" />
+          </SkillIconContainer>
+        </div>
+      </SectionBody>
+    </SectionContainer>
+  );
+};
 
 export default Skills;
